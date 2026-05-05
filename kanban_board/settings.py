66a1879915fa5ad293d850926f39cc85695a7747
Avishekpaul1310 <avishekpaul1310@gmail.com>
2026-05-05 08:21:30 +0530

    feat: Enhance Google OAuth integration with improved login handling and user prompts

--- kanban_board/settings.py
+++ kanban_board/settings.py
@@ -323,6 +323,7 @@ SOCIALACCOUNT_PROVIDERS = {
         ],
         'AUTH_PARAMS': {
             'access_type': 'online',
+            'prompt': 'select_account',
         },
         'OAUTH_PKCE_ENABLED': True,
         'FETCH_USERINFO': True,
